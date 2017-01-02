fdfcd2f6b5e557cba41182310c5f6c89e2753d00
21mad21 <mad.21@live.fr>
2017-01-02 15:12:06 -0500

    initial commit

---
+++ clean for ecplise.bat
@@ -0,0 +1,2 @@
+call gradlew cleanEclipse
+call gradlew eclipse
---
+++ desktop/src/fr/mad/kassu/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.mad.kassu.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.mad.kassu.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
